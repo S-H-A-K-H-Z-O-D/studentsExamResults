e2532ba20815824bd714c28cd0980342cf140648
S-H-A-K-H-Z-O-D <M.Shakhzod2002@gmail.com>
2023-04-27 12:51:22 +0500

    Students exam project is completed

--- js/data.js
+++ js/data.js
@@ -4,34 +4,34 @@ let students = localStorage.getItem("students") ? JSON.parse(localStorage.getIte
     name: "Nurmuhammad",
     lastName: "Mahmudov",
     mark: 140.5,
-    markedDate: new Date("2021-12-05").toISOString()
+    markedDate: new Date("2021-12-05")
   },
   {
     id: 101,
     name: "Asadbek",
     lastName: "Asadov",
     mark: 146,
-    markedDate: new Date("2021-12-06").toISOString()
+    markedDate: new Date("2021-12-06")
   },
   {
     id: 102,
     name: "Ahmadjon",
     lastName: "Hasanjanov",
     mark: 75,
-    markedDate: new Date("2021-12-01").toISOString()
+    markedDate: new Date("2021-12-01")
   },
   {
     id: 103,
     name: "G'anivoy",
     lastName: "Teshayev",
     mark: 40,
-    markedDate: new Date("2021-12-05").toISOString()
+    markedDate: new Date("2021-12-05")
   },
   {
     id: 104,
     name: "Kamronbek",
     lastName: "Zoirov",
     mark: 150,
-    markedDate: new Date("2022-03-29").toISOString()
+    markedDate: new Date("2022-03-29")
   }
 ]
--- js/main.js
+++ js/main.js
@@ -14,9 +14,52 @@ let elEditMark = document.getElementById('edit-mark');
 //    OTHER VAREABLES
 let elCount = document.querySelector(".count");
 let elAverageMark = document.querySelector(".average-mark");
-//    VAREABLES OF SEARCHING
+//    VAREABLES OF SEARCHING FILTER
 let elFilter = document.querySelector('.filter');
 let elSearch = document.getElementById('search');
+//    VAREABLES OF FILTER BY MARK
+let elFrom = document.getElementById('from');
+let elTo = document.getElementById('to');
+//    VAREABLES OF FILTER BY SORTING
+let elSort = document.getElementById('sortby');
+
+
+Object.prototype.toMakeDate = function () {
+
+     let day = String(this.getDate()).padStart(2, 0);
+     let month = String(this.getMonth()+1).padStart(2, 0);
+     let year = this.getFullYear();
+
+     return `${day}/${month}/${year}`;
+};
+
+let sortFn = {
+     az: (a, b) => {
+          if(a.name > b.name){
+               return 1
+          }
+
+          if(a.name < b.name){
+               return -1
+          }
+
+          return 0
+     },
+     za: (a, b) => {
+          if(a.name > b.name){
+               return -1
+          }
+
+          if(a.name < b.name){
+               return 1
+          }
+
+          return 0
+     },
+     markToLow: (a, b) => b.mark - a.mark,
+     markToHigh: (a, b) => a.mark - b.mark,
+     date: (a, b) => a.markedDate.getTime() - b.markedDate.getTime(),
+}
 
 function count(arr){
      let a = 0;
@@ -45,7 +88,7 @@ function renderStudents(arr){
          elStudentRowWrapper.dataset.id = element.id;
          studentId.textContent = element.id;
          studentName.innerHTML = `${element.name} ${element.lastName}`;
-         studentMarkedDate.textContent = element.markedDate;
+         studentMarkedDate.textContent = element.markedDate.toMakeDate();
          studentMark.textContent = element.mark;
          
          if(element.mark >= 104){
@@ -68,37 +111,33 @@ function addStudent(evt){
           name: elName.value,
           lastName: elLastname.value,
           mark: elMark.value,
-          markedDate: new Date("2021-12-01").toISOString()
+          markedDate: new Date()
      }
 
      students.push(student);
      renderStudents(students);
      count(students)
 }
-function beforeEdit(id){
 
-     let onEdit = (e) => {
-          e.preventDefault();
+let elEditId = null
 
-          // console.log(e.target)
-          students.forEach(el => {
-               if(el.id == id){
-                    el.name = elEditName.value;
-                    el.lastName = elEditLastname.value;
-                    el.mark = elEditMark.value;
-               }
-          })
-          renderStudents(students);
-          count(students);
-          console.log(students)
-     }
-     
-     elEditForm.addEventListener('submit', onEdit);
+let onEdit = (e) => {
+     e.preventDefault();
+
+     students.forEach(el => {
+          if(el.id == elEditId){
+               el.name = elEditName.value;
+               el.lastName = elEditLastname.value;
+               el.mark = elEditMark.value;
+          }
+     })
+     renderStudents(students);
+     count(students);
 }
 
-function beforeDelete(id){
+function onDelete(id){
      let delArr = [];
-
+     
      students.forEach(el => {
           if(el.id != id){
                delArr.push(el);
@@ -115,39 +154,40 @@ function onFix(evt){
           
           students.forEach(el => {
                if(el.id == evt.target.closest('tr').dataset.id){
-                   
+                    
                     elEditName.value = el.name;   
                     elEditLastname.value = el.lastName; 
                     elEditMark.value = el.mark; 
                }
           })
-          beforeEdit(evt.target.closest('tr').dataset.id);
+          
+          elEditId = evt.target.closest('tr').dataset.id
      };
 
      if(evt.target.matches('.student-delete')){
-          beforeDelete(evt.target.closest('tr').dataset.id);
+          onDelete(evt.target.closest('tr').dataset.id);
      }
 }
 
 function onFilter(evt){
      evt.preventDefault();
-
+     
      let searchValue = elSearch.value.trim();
-
-     if(!searchValue){
-          return alert('Input smth to search!');
-     }
-
+     
      let regex = new RegExp(searchValue, 'gi');
-
+     
      let filteredStudents = [];
-
-     students.forEach((std) => {          
-
+     
+     students.forEach((std) => {  
+          
+          if(!searchValue){
+               return filteredStudents.push(std);
+          }
+          
           if(`${std.name} ${std.lastName}`.match(regex)){
-
+               
                let a = (`${std.name} ${std.lastName}`.match(regex)[0]);
-
+               
                filteredStudents.push({
                     ...std,
                     name: `${std.name} ${std.lastName}`.replace(regex, `<span class="bg-primary">${a}</span>`),
@@ -156,10 +196,20 @@ function onFilter(evt){
           }
      });
 
+     if(elFrom.value && elTo.value){
+
+          filteredStudents = filteredStudents.filter(std => std.mark >= Number(elFrom.value) && std.mark <= Number(elTo.value));
+     };
+
+     if(elSort.value){
+          filteredStudents.sort(sortFn[elSort.value]);
+     }
+     
      renderStudents(filteredStudents);
      count(filteredStudents)
 }
 
+elEditForm.addEventListener('submit', onEdit);
 elFilter.addEventListener('submit', onFilter)
 elTableBody.addEventListener('click', onFix);
 elAddForm.addEventListener('submit', addStudent);
